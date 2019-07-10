1b32e39b1a145da34584940085c0b3fe5b0046bf
Robin G <50486040+robio10000@users.noreply.github.com>
2019-07-10 14:55:26 +0200

    Add files via upload

--- TakeRoot/Main.java
+++ TakeRoot/Main.java
@@ -1,67 +1,82 @@
 package main;
 
+import java.util.Scanner;
+
 public class Main {
 
-	public static void main(String[] args) throws InterruptedException {
-		double input = 3.3;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 
+		System.out.printf("#: ");
+		double input = sc.nextDouble(); // Keyboard input
 
-		float out = (float) takeRoot(input, 0, input, 1E-20, new Square());
-		float rightResult = (float) Math.sqrt(input);
-		System.out.println("MY:" + out + "MATH:" + rightResult);
-		if (out == rightResult) {
-			System.out.println(true);
-		} else {
-			System.out.println(false);
-		}
+		sc.close();
+
+		double out = takeRoot(input, 0, input, 0, x -> x * x); // Use the method
+		double rightResult = Math.sqrt(input);
+		System.out.printf("\nResult: MY: %f MATH: %f\n", out, rightResult);
 
 	}
 
+	/**
+	 * Calculate with Binary search
+	 * 
+	 * @param inp   your number
+	 * @param min   minimum of search area
+	 * @param max   maximum of search area
+	 * @param acc   accuracy
+	 * @param iFunc f(x)
+	 * @return the result of Binary search with your f(x)
+	 */
 	private static double takeRoot(double inp, double min, double max, double acc, IFunction iFunc) {
-		double mid = 0d;
-		double prev = mid;
-		double diff2 = 1;
 
-		if (inp < 0) { // If the input is for example -1
-			throw new IllegalArgumentException("Input under 0 is not allowed");
+		// Check the parameters
+		if (inp < 1 && (max < 1 || max == inp)) { // If the input is for example 0.9 and if your max is not right
+			throw new IllegalArgumentException("Please check your arguments!(min, max)");
 		}
-		if (inp == 0) {
-			return 0;
-		} else if (inp == 1) {
-			return 1;
+		if (acc <= 0) { // If the accuracy is 0 or smaller
+			throw new IllegalArgumentException("Please check your arguments!(accuracy)");
 		}
-		if (inp < 1) {
-//			System.out.println("Your input is under 1. Min and max was changed!");
-			min = 0;
-			max = 1;
-		}
+
+		double midOld = Double.NaN;
 
 		while (true) {
-			mid = (min + max) / 2;
+
+//			double diff = Math.abs(iFunc.function(min) - inp);
+//			if (diff <= acc) { // Checks if min or max is output
+//				return min;
+//			}
+//			diff = Math.abs(iFunc.function(max) - inp);
+//			if (diff <= acc) {
+//				return max;
+//			}
+
+			double mid = (min + max) / 2; // set the middle
 			if (iFunc.function(mid) < inp) {
 				min = mid;
 			} else {
 				max = mid;
 			}
+
 			double diff = Math.abs((iFunc.function(mid)) - inp); // calculate the difference between the middle and the
 																	// input
-
-			if (diff <= acc || iFunc.function(mid) == inp) { // if the difference between middle and the input is not
-																// more than 1E-x
-//				System.out.println("diff < acc");
-				break;
+			if (diff <= acc) { // if the difference between middle and the input is not
+								// more than 1E-x
+				return mid;
 			}
-			diff2 = Math.abs(prev - mid); // the difference between previous middle and middle
-//			 System.out.println(diff2);
-			if (diff2 == 0) {
-				break;
-			}
+			diff = max - min;
 
-//			System.out.println(mid);
-			prev = mid;
+			// Checks if the difference between max and min is fit or (minOld has a value
+			// and the difference between midOld and mid is fit)
+			if (diff <= acc || (!Double.isNaN(midOld) && Math.abs(midOld - mid) < acc)) {
+				System.out.println(max + " - " + min + " = " + (max - min) + " <= " + acc);
+				System.out.println("f(" + mid + ") = " + mid * mid);
+//				throw new IllegalArgumentException("Please check your arguments!");
+				return mid;
+			}
+			midOld = mid;
 
 		}
-		return mid;
 	}
 
 }
